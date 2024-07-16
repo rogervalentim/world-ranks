5375728b35a1060626eb5ccdf04a2f9dee11a3df
rogervalentim <valentim.roger33@gmail.com>
2024-07-15 21:24:03 -0300

    feat: add button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,15 @@
 interface ButtonProps {
   continent: string;
+  onClick: () => void;
+  selected: boolean;
 }
 
-export function Button({ continent }: ButtonProps) {
+export function Button({ continent, selected, onClick }: ButtonProps) {
   return (
-    <button className="rounded-xl px-3 py-2 text-xs font-medium text-[#6C727F] hover:bg-[#282B30] hover:text-[#D2D5DA]">
+    <button
+      onClick={onClick}
+      className={`pl- rounded-xl px-1 py-2 text-xs font-medium ${selected ? "bg-[#282B30] text-[#D2D5DA]" : ""} text-[#6C727F] hover:bg-[#282B30] hover:text-[#D2D5DA] lg:px-3`}
+    >
       {continent}
     </button>
   );
