992ad38588742d55fdd78193d7fde97c54445c12
rogervalentim <valentim.roger33@gmail.com>
2024-07-15 21:24:57 -0300

    feat: add types

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface CountryData {
+  flags: {
+    alt: string;
+    png: string;
+  };
+  name: {
+    common: string;
+  };
+  population: number;
+  area: number;
+  independent: boolean;
+  unMember: boolean;
+  region: string;
+}
